f54008ab0ce98938fe7278f89f47135fa932433f
Tanish Tyagi <38384138+anaconda121@users.noreply.github.com>
2021-11-24 09:45:33 -0500

    end of fall term update commit 2

--- EDA/Proposed Model Matrix.md
+++
@@ -1,2 +0,0 @@
-| Patient Id      | Content | Label        | 
-| :---            |   :----:|         ---: |     
--- nlp_annotator-dev/app/gui/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase, Client, RequestFactory
-from django.shortcuts import reverse
-from accounts.models import CustomUser
-from gui.models import PatientDemographic
-from gui.views import annotator_home, user_profile
-
-
-class GuiViewsTests(TestCase):
-    pass
