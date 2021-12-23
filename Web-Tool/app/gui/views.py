40b7ea5a29114a12e3793e1aaf82380162e8f05f
Tanish Tyagi <38384138+anaconda121@users.noreply.github.com>
2021-12-23 16:09:46 -0500

    finished up to merged context windows

--- Web-Tool/app/gui/views.py
+++ Web-Tool/app/gui/views.py
@@ -1,3 +1,5 @@
+# Author: Tanish Tyagi
+
 from django.shortcuts import render
 
 import pandas as pd
@@ -21,6 +23,9 @@ def compile_regex():
     return p_list 
 
 def read_notes(note_txt):
+    """
+    note_txt: file path that contains notes
+    """
     # dataframe to save notes to 
     note_df = pd.DataFrame(columns = ["NoteTXT"])
 
@@ -38,6 +43,10 @@ def read_notes(note_txt):
     return note_df 
 
 def find_matches(notes, keywords):
+    """
+    notes: dataframe
+    keywords: list of compiled regex expressions for 18 dementia-related keywords
+    """
     # array for storing keyword matches
     matches = [] 
 
@@ -55,9 +64,95 @@ def find_matches(notes, keywords):
 
     return matches 
 
+def find_character_locations(notes, keywords):
+    """
+    notes: dataframe 
+    keywords: list of compiled regex expressions for 18 dementia-related keywords
+    """
+    # array for storing character locations of matches; list of tuples
+    locations = []
+
+    for r in (notes['NoteTXT']):
+        curr_loc = []
+
+        # finding character locations
+        for p in keywords:
+            for match in re.finditer(p ,r):
+                curr_loc.append(match.span())
+                
+        # TODO: decide if need to keep this code
+        # current logic; if no character location is found, something is wrong
+        if (curr_loc == []):
+            print("STOP")
+            break
+            
+        locations.append(curr_loc)
+    
+    return locations 
+
+# TODO: This function may not be needed if rows that have no keyword matches are kept 
+def create_context_windows(notes, i):
+    """
+    notes: dataframe 
+    i: int that represents index in notes
+    """
+    merged = []
+    context_length_one_direction = 100
+    
+    for j in range(len(notes["regex_location"][i])):
+        start = notes["regex_location"][i][j][0]
+        end = notes["regex_location"][i][j][1]
+
+        # creating context windows
+        prune_start = False
+        prune_end = False
+
+        if (start - context_length_one_direction <= 0):
+            start = 0
+            prune_start = True
+        if (end + context_length_one_direction >= len(notes["NoteTXT"][i]) - 1):
+            end = len(notes["NoteTXT"][i]) - 1
+            prune_end = True 
+
+        if (prune_start and prune_end):
+            merged.append((start, end))
+        else:
+            if (prune_start):
+                merged.append((start, end + context_length_one_direction))
+            if (prune_end):
+                merged.append((start - context_length_one_direction, end))
+
+        if (not prune_start and not prune_end):
+            merged.append((start - context_length_one_direction, end + context_length_one_direction))
+    
+    return merged
+
+def merge(intervals):  
+    """
+    intervals: list of tuples representing context windows 
+    """
+    ans = []
+    i = 1
+    intervals.sort()
+    tempans = intervals[0]
+    
+    while i < len(intervals):
+        # seeing if context windows overlap with each other - if they do merge them together
+        if tempans[0] <= intervals[i][1] and tempans[1] >= intervals[i][0]:
+            newtempans = [min(tempans[0] , intervals[i][0]) , max(tempans[1] , intervals[i][1])]
+            tempans = newtempans
+            i += 1
+        else:
+            ans.append(tempans)
+            tempans = intervals[i]
+            i += 1
+            
+    ans.append(tempans)
+    return ans
+
 def sequence_extraction_pipeline(note_txt):
     """
-    note_txt: file path (?) that contains all notes, notes are separated by a line
+    note_txt: file path (TODO: decide this?) that contains all notes, notes are separated by a line
     """
     # TODO: clean up extraction pipeline from web form 
     notes = read_notes(note_txt)
@@ -69,10 +164,28 @@ def sequence_extraction_pipeline(note_txt):
     matches = find_matches(notes, keywords)
     notes["regex_matches"] = matches  
 
+    # TODO: decide if drop rows that do not have regex match -- I think no?
+
+    # getting character locations for notes
+    character_locations = find_character_locations(notes, keywords)
+    notes["regex_location"] = character_locations 
+
+    # creating context windows
+    context_windows = []
+    for i in range(len(notes)):
+        context_windows.append(create_context_windows(notes, i))
+    notes["context_windows"] = context_windows
+
+    # merging context windows on note level
+    merged = []
+    for i in range(len(notes)):
+        merged.append(merge(notes["context_windows"][i]))
+    notes["merged_row_location"] = merged 
+
     return notes 
 
 def tester():
     notes = sequence_extraction_pipeline(r"C:\Users\tanis\OneDrive - Phillips Exeter Academy\Data\Programming\APOE-SLAT\Web-Tool\app\gui\static\test_notes.txt")
-    print(notes["regex_matches"]) 
+    print(notes["merged_row_location"]) 
 
 tester()
