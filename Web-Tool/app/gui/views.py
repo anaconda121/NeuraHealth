6dd2617e410befd90b866a063ffa6aecd1a2cdeb
Tanish Tyagi <38384138+anaconda121@users.noreply.github.com>
2021-12-29 10:19:19 -0500

    web portal clinical bert model integrated!

--- Web-Tool/app/gui/views.py
+++ Web-Tool/app/gui/views.py
@@ -2,9 +2,19 @@
 
 from django.shortcuts import render
 
-def generate_output(notes):
+import pandas as pd
+
+from sequence_extraction_pipeline import sequence_extraction_pipeline
+from run_clinical_bert import run_cb
+
+def generate_output():
     """
     notes: dataframe with seq data and model preds
     """
-    
-    
+    path = r"C:\Users\tanis\OneDrive - Phillips Exeter Academy\Data\Programming\APOE-SLAT\Web-Tool\app\gui\static\test_notes.txt"
+    notes = sequence_extraction_pipeline(path)
+    notes = notes.rename(columns = {'regex_sent': 'text'}) 
+    notes = run_cb(notes)
+    notes.to_csv(r"static/test_preds.csv", index = False)
+
+generate_output()
