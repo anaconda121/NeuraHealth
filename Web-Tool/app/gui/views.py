7bdc447538efd7e615a7d7342a7abcdea200931b
Tanish Tyagi <38384138+anaconda121@users.noreply.github.com>
2021-12-29 19:51:57 -0500

    integrated patient level model into backend

--- Web-Tool/app/gui/views.py
+++ Web-Tool/app/gui/views.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from sequence_extraction_pipeline import sequence_extraction_pipeline
 from run_clinical_bert import run_cb
+from patient_level_model import run_patient_level
 
 def generate_output():
     """
@@ -16,5 +17,7 @@ def generate_output():
     notes = notes.rename(columns = {'regex_sent': 'text'}) 
     notes = run_cb(notes)
     notes.to_csv(r"static/test_preds.csv", index = False)
+    patient_level_features = run_patient_level(notes)
+    patient_level_features.to_csv(r"static/patient_level_pred.csv", index = False)
 
 generate_output()
